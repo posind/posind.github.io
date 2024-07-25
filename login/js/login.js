f2c8de0277c409d3e6c09890164ac27cad26f217
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-07-25 14:51:57 +0700

    membuat link untuk login dan register agar tersimpan ke database

--- login/js/login.js
+++ login/js/login.js
@@ -13,7 +13,7 @@ document
 
     try {
       const response = await fetch(
-        "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/user",
+        "https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/endpoint_user/user",
         {
           method: "POST",
           headers: {
