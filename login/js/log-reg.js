555307c22f51205784756aead959982226dbec11
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-07-29 11:37:18 +0700

    perbaiki fungsi js log dan regis

--- login/js/log-reg.js
+++ login/js/log-reg.js
@@ -1,4 +1,4 @@
-// login
+// Login
 document
   .getElementById("loginForm")
   .addEventListener("submit", async function (e) {
@@ -36,66 +36,70 @@ document
       console.error("Error:", error);
       document.getElementById("error").style.display = "block";
     }
-  });// login
+  });
 
-//   register
-  document.addEventListener("DOMContentLoaded", function () {
-    const registerButton = document.getElementById("registerButton");
-  
-    registerButton.addEventListener("click", function (event) {
-      event.preventDefault();
-  
-      // Ambil nilai input dari form
-      const username = document.getElementById("username").value;
-      const email = document.getElementById("email").value;
-      const password = document.getElementById("password").value;
-  
-      // Siapkan data untuk dikirim ke server
-      const data = {
-        username: username,
-        email: email,
-        password: password,
-      };
-  
-      // Kirim data ke server menggunakan fetch
-      fetch(
-        "https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
+// Register
+document.addEventListener("DOMContentLoaded", function () {
+  const registerButton = document.getElementById("registerButton");
+
+  registerButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Ambil nilai input dari form
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Siapkan data untuk dikirim ke server
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    // Kirim data ke server menggunakan fetch
+    fetch(
+      "https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
         }
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.success) {
-            alert("Registrasi Berhasil");
-            // Reset form setelah registrasi berhasil
-            document.getElementById("username").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("password").value = "";
-          } else {
-            alert("Registration : " + data.message);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-          alert("Terjadi kesalahan. Silakan coba lagi nanti.");
-        });
-    });//   register
-  
-    // Event listener untuk checkbox "Show Password"
-    document
-      .getElementById("show-password")
-      .addEventListener("change", function () {
-        const password = document.getElementById("password");
-        if (this.checked) {
-          password.type = "text";
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          alert("Registrasi Berhasil");
+          // Reset form setelah registrasi berhasil
+          document.getElementById("username").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("password").value = "";
         } else {
-          password.type = "password";
+          alert("Registration : " + data.message);
         }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Terjadi kesalahan. Silakan coba lagi nanti.");
       });
   });
-  
+
+  // Event listener untuk checkbox "Show Password"
+  document
+    .getElementById("show-password")
+    .addEventListener("change", function () {
+      const password = document.getElementById("password");
+      if (this.checked) {
+        password.type = "text";
+      } else {
+        password.type = "password";
+      }
+    });
+});
